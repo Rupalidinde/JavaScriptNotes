26d88d3d40af9199d94e77f74aeda2f20ec2db03
gambitier <csegambitier@gmail.com>
2022-02-22 19:47:43 +0530

    add proto chaining

--- session 1/server/index.js
+++ session 1/server/index.js
@@ -1,17 +1,56 @@
-function Foo() {}
+//SuperType constructor function
+function SuperType() {
+	this.name = 'Virat';
+}
 
-const dunderProtoEqualsPrototypeOfFunction =
-	new Foo().__proto__ === Foo.prototype;
+//SuperType prototype
+SuperType.prototype.getSuperName = function () {
+	return this.name;
+};
 
-const actualObjectDontHavePrototype = new Foo().prototype === undefined;
+//SubType prototype function
+function SubType() {
+	this.age = 26;
+}
 
-console.log(
-	'dunder Proto Equals Prototype Of Function',
-	dunderProtoEqualsPrototypeOfFunction
-);
+//Inherit the properties from SuperType
+SubType.prototype = new SuperType();
 
-console.log('actual Object Dont Have Prototype', actualObjectDontHavePrototype);
+//Add new property to SubType prototype
+SubType.prototype.getSubAge = function () {
+	return this.age;
+};
 
-const fooObject = new Foo();
-console.log('fooObject is instance of Foo', fooObject instanceof Foo);
-console.log('fooObject is instance of Object', fooObject instanceof Object);
+//Create a SubType object
+var subTypeObj = new SubType();
+console.log('subTypeObj.name', subTypeObj.name); //Output: Virat
+console.log('subTypeObj.age', subTypeObj.age); //Output: 26
+console.log('subTypeObj.getSuperName()', subTypeObj.getSuperName()); //Output: Virat
+console.log('subTypeObj.getSubAge()', subTypeObj.getSubAge()); //Output: 26
+
+console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+
+var subTypeObj2 = new SubType();
+console.log('2-subTypeObj.name', subTypeObj2.name); //Output: Virat
+console.log('2-subTypeObj.age', subTypeObj2.age); //Output: 26
+console.log('2-subTypeObj.getSuperName()', subTypeObj2.getSuperName()); //Output: Virat
+console.log('2-subTypeObj.getSubAge()', subTypeObj2.getSubAge()); //Output: 26
+
+console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+
+subTypeObj2.name = 'akash';
+console.log('=======after changing the name property==========');
+console.log('2-subTypeObj.name', subTypeObj2.name); //Output: Virat
+console.log('subTypeObj.name', subTypeObj.name); //Output: Virat
+console.log('2-subTypeObj.getSuperName()', subTypeObj2.getSuperName()); //Output: Virat
+console.log('subTypeObj.getSuperName()', subTypeObj.getSuperName()); //Output: Virat
+
+console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+
+var newObj = new SuperType();
+newObj.__proto__.getSuperName = () => {
+	return 'changed function definition';
+};
+
+console.log('2-subTypeObj.getSuperName()', subTypeObj2.getSuperName()); //Output: Virat
+console.log('subTypeObj.getSuperName()', subTypeObj.getSuperName()); //Output: Virat
