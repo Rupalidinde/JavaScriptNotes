4c8585cda2a89cdf1f875b6ac4af78bd54503590
gambitier <csegambitier@gmail.com>
2022-02-23 19:23:19 +0530

    export more methods

--- server/calculatorCommonjs.js
+++ server/calculatorCommonjs.js
@@ -6,7 +6,16 @@ function multiply(a, b) {
 	return a * b;
 }
 
+var division = (a, b) => {
+	return a / b;
+};
+
+function secretFunction(a, b) {
+	return `a - b = ${a - b}`;
+}
+
 module.exports = {
 	add,
 	multiply,
+	division,
 };
--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
 var calculator = require('./calculatorCommonjs');
 
 console.log(calculator.add(12, 3));
+
+console.log(calculator.division(12, 3));
+
+console.log(calculator.secretFunction(12, 3));
